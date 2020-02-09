d6bb406b52265f1a36d3b017d386daaa609e826d
Dhruvil Dave <dhruvil.dave@outlook.com>
2020-02-09 05:39:11 +0530

    Sun 09 Feb 2020 05:39:11 AM IST

--- model.py
+++ model.py
@@ -5,14 +5,15 @@ import string
 from pprint import pprint
 from typing import Dict, List
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow.keras as keras
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from tensorflow.keras import layers, models
-from tensorflow.keras.preprocessing import text, sequence
-from tensorflow.keras.utils import to_categorical, plot_model
+from tensorflow.keras.preprocessing import sequence, text
+from tensorflow.keras.utils import plot_model, to_categorical
 
 
 def preprocess(sentence: str) -> str:
@@ -51,166 +52,8 @@ def scale_scores(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def create_word_index(df: pd.DataFrame, vocab: str) -> Dict[str, float]:
-    '''Return 0 to 1 index of vocabulary'''
-
-    words = [x.split() for x in df[vocab]]
-
-    res = []
-    [res.extend(x) for x in words]
-    res = set(res)
-
-    index = {}
-    for i in enumerate(res, start=1):
-        # index[i[1]] = i[0] / len(res)
-        index[i[1]] = i[0]
-
-    return index
-
-
-def model(train: pd.DataFrame, test: pd.DataFrame) -> models.Sequential:
-    about_model = models.Sequential()
-    about_model.add(
-        layers.Embedding(input_dim=91, output_dim=200, input_length=91))
-    about_model = layers.Input(shape=[91], name='about_model')
-    ing_model = layers.Input(shape=[56], name='ing_model')
-
-    x = layers.Embedding(input_dim=91, output_dim=200,
-                         input_length=91)(about_model)
-    x = layers.Dense(200, activation='relu', kernel_initializer='he_normal')(x)
-    about_model_out = layers.Dense(100, activation='elu')(x)
-
-    x = layers.Embedding(input_dim=56, output_dim=120,
-                         input_length=56)(ing_model)
-    x = layers.Dense(120, activation='relu', kernel_initializer='he_normal')(x)
-    ing_model_out = layers.Dense(100,
-                                 activation='relu',
-                                 kernel_initializer='he_normal')(x)
-
-    about_ing_concat = layers.concatenate([about_model_out, ing_model_out],
-                                          axis=1)
-    about_ing_concat = layers.Flatten()(about_ing_concat)
-
-    cat_model = layers.Input(shape=[19], name='cat_model')
-
-    final = layers.concatenate([cat_model, about_ing_concat], axis=-1)
-    final = layers.Dense(1,
-                         name='output',
-                         activation='relu',
-                         kernel_initializer='he_normal')(final)
-
-    model = models.Model(inputs=[about_model, ing_model, cat_model],
-                         outputs=[final])
-
-    model.compile('rmsprop', 'mse', metrics=['accuracy'])
-
-    rand_df = pd.DataFrame(np.random.randn(100, 2))
-    msk = np.random.rand(len(train)) < 0.6
-
-    x_train = train[msk]
-    print(x_train.shape)
-    x_valid = train[~msk]
-    print(x_valid.shape)
-
-    x = x_train['about']
-    print(x.iloc[0].shape)
-    x1 = x_train['ing']
-    print(x1.shape)
-    x2 = x_train['category']
-    print(x2.shape)
-
-    xvalid = x_valid['about']
-    x1valid = x_valid['ing']
-    x2valid = x_valid['category']
-
-    y_train = x_train['score']
-    y_valid = x_valid['score']
-
-    # plot_model(model, to_file='model.png')
-    hist = model.fit(
-        (np.asarray(x), np.asarray(x1), np.asarray(x2)),
-        np.asarray(y_train),
-        epochs=20,
-        validation_data=((np.asarray(xvalid), np.asarray(x1valid),
-                          np.asarray(x2valid)), np.asarray(y_valid)))
-    return model
-
-
-def main() -> None:
-    df = pd.read_csv('dataset.csv')
-    df.drop('Unnamed: 0', axis=1, inplace=True)
-
-    about_index = create_word_index(df, 'about')
-
-    num = max([len(x.split()) for x in df['about']])
-    print(num)
-    L = []
-    for i in range(df.shape[0]):
-        l: List[str] = df.iloc[i]['about'].split()
-        for j in enumerate(l):
-            l[j[0]] = about_index[j[1]]
-        l.extend([0] * (num - len(l)))
-        L.append(l)
-
-    df = pd.DataFrame({
-        'name': df['name'],
-        'category': df['category'],
-        'score': df['score'],
-        'ing': df['ing'],
-        'about': L
-    })
-
-    ing_index = create_word_index(df, 'ing')
-    num = max([len(x.split()) for x in df['ing']])
-    print(num)
-    L = []
-    for i in range(df.shape[0]):
-        l: List[str] = df.iloc[i]['ing'].split()
-        for j in enumerate(l):
-            l[j[0]] = ing_index[j[1]]
-        l.extend([0] * (num - len(l)))
-        L.append(l)
-
-    df = pd.DataFrame({
-        'name': df['name'],
-        'category': df['category'],
-        'score': df['score'],
-        'ing': L,
-        'about': df['about']
-    })
-
-    hist = {}
-    for i in enumerate(set(df['category'])):
-        hist[i[1]] = i[0]
-    L = []
-    for i in range(df.shape[0]):
-        l = to_categorical(hist[df.iloc[i]['category']], num_classes=len(hist))
-        L.append(l)
-
-    df = pd.DataFrame({
-        'name': df['name'],
-        'category': L,
-        'score': df['score'],
-        'ing': df['ing'],
-        'about': df['about']
-    })
-
-    df['ing_np'] = None
-    for i in range(len(df['ing_np'])):
-        df['ing_np'][i] = np.array(df['ing_np'])
-
-    print(df['ing_np'])
-    rand_df = pd.DataFrame(np.random.randn(100, 2))
-    msk = np.random.rand(len(df)) < 0.8
-    train = df[msk]
-    test = df[~msk]
-    # mdl = model(train, test)
-    print(train[['ing', 'about', 'score', 'category']])
-
-    print(df.describe())
-
-
-def new_model(ing, about, category, score):
+def new_model(ing: np.ndarray, about: np.ndarray, category: np.ndarray,
+              score: np.ndarray) -> models.Model:
     inputA = layers.Input(shape=91, name='about_model')
     inputB = layers.Input(shape=56, name='ing_model')
     inputC = layers.Input(shape=19, name='cat_model')
@@ -246,7 +89,35 @@ def new_model(ing, about, category, score):
 
     model = models.Model(inputs=[inputA, inputB, inputC], outputs=[final])
     model.compile('rmsprop', 'mse', metrics=['accuracy'])
-    hist = model.fit([ing, about, category], score, epochs=500)
+    hist = model.fit([ing, about, category], score, epochs=50, batch_size=16)
+
+    res = model.evaluate([ing, about, category], score)
+    pred = model.predict([ing, about, category])
+    print(pred)
+    plt.plot(pred, label='pred')
+    plt.plot(score, label='score')
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+    return model
+
+
+def ing_model(ing, score) -> None:
+    model = models.Sequential()
+    model.add(
+        layers.Dense(56,
+                     activation='relu',
+                     input_shape=(56, ),
+                     kernel_initializer='he_normal'))
+    model.add(
+        layers.Dense(200, activation='relu', kernel_initializer='he_normal'))
+    model.add(
+        layers.Dense(100, activation='relu', kernel_initializer='he_normal'))
+    model.add(
+        layers.Dense(1, activation='relu', kernel_initializer='he_normal'))
+    model.compile('rmsprop', 'mse', metrics=['accuracy'])
+    hist = model.fit([ing], score, epochs=50, batch_size=1024)
 
     return model
 
@@ -257,19 +128,15 @@ def new() -> None:
         spamreader = csv.DictReader(csvfile)
         name = [row['name'] for row in spamreader]
 
-    with open('dataset.csv', newline='') as csvfile:
         spamreader = csv.DictReader(csvfile)
         ing = [row['ing'] for row in spamreader]
 
-    with open('dataset.csv', newline='') as csvfile:
         spamreader = csv.DictReader(csvfile)
         about = [row['about'] for row in spamreader]
 
-    with open('dataset.csv', newline='') as csvfile:
         spamreader = csv.DictReader(csvfile)
         category = [row['category'] for row in spamreader]
 
-    with open('dataset.csv', newline='') as csvfile:
         spamreader = csv.DictReader(csvfile)
         score = [row['score'] for row in spamreader]
 
@@ -283,23 +150,17 @@ def new() -> None:
     ing = sequence.pad_sequences(ing, maxlen=56, padding='post')
     about = sequence.pad_sequences(about, maxlen=91, padding='post')
 
-    # print(about[0])
-
     hist = {}
     category = set(category)
-    # print(category)
 
     for i in enumerate(category):
         hist[i[1]] = i[0]
-    # print(hist)
 
     category = np.array(
         [np.array(to_categorical(hist[arr[1, i]], 19)) for i in range(357)])
-    # print(category)
 
     score = np.array([float(x) for x in score])
 
-    # print(type(score[0]))
     mdl = new_model(about, ing, category, np.array(score))
 
 
